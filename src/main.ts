1f45d3c1076cc9e818a2fbf678725780403a89a7
wxicatherine <kateryna.hrytsak.pm.2023@lpnu.ua>
2025-04-05 17:38:15 +0300

    main ts

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.enableCors({
+    origin: 'http://localhost:4200', // Замініть на ваш фронтенд
+    methods: 'GET,POST,DELETE',
+    credentials: true,
+  });
+  await app.listen(3000);
 }
 bootstrap();
